a6d9a8f84b6e57808aee2ed90fdfcd3e9bba9fb4
jooyeop <royalmansu@gmail.com>
2022-09-07 10:51:13 +0900

    Idol-classification

--- Kpop-Idol-Classification.py
+++ Kpop-Idol-Classification.py
@@ -15,3 +15,14 @@ from monai.networks.nets import DenseNet121,EfficientNetBN
 from monai.transforms import *
 from monai.data import Dataset, DataLoader
 from monai.utils import set_determinism
+
+train=pd.read_csv('../input/kidf-kpop-idol-dataset-female/kid_f_train.csv')
+test=pd.read_csv('../input/kidf-kpop-idol-dataset-female/kid_f_test.csv')
+
+class_names0=train['name'].unique().tolist()
+tclass_names0=test['name'].unique().tolist()
+available_class=list(set(class_names0)&set(tclass_names0))
+
+
+train = []
+testi = []
